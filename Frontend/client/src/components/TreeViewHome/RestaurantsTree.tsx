8cacffc14f996b03a0275acb647859d9a24f12d5
Elindrae <sarah.metzger@viacesi.fr>
2024-06-19 14:58:21 +0200

    Page restaurant de Client, link back

--- Frontend/client/src/components/TreeViewHome/RestaurantsTree.tsx
+++ Frontend/client/src/components/TreeViewHome/RestaurantsTree.tsx
@@ -2,42 +2,43 @@ import { Card, CardActionArea, CardContent, CardMedia, Grid, Typography } from "
 import burgerImg from '../../Assets/burger.jpg';
 import axios from "axios";
 import { useQuery } from "react-query";
+import { useNavigate } from "react-router-dom";
 
 export default function RestaurantsTree() {
-
     const fetchRestaurants = async () => {
         const {data} = await axios.get('http://localhost/api/restaurants');
         return data;
     }
  
     const { data, isLoading } = useQuery('restaurants', fetchRestaurants);
+    const navigate = useNavigate();
 
     return (
             <Grid container sx={{ justifyContent: 'center' }}>
                 {data?.map((restaurant: any) => (
-                <Grid key={restaurant._id} item xs={3} sx={{ mx:6, my: 2 }}>
-                    <Card>
-                        <CardActionArea>
-                            <CardMedia
-                            component="img"
-                            height="140"
-                            image={burgerImg}
-                            alt="Burger King"
-                            />
-                            <CardContent>
-                                <Typography gutterBottom variant="h5" component="div">
-                                    {restaurant.name}
-                                </Typography>
-                                <Typography variant="body2" color="text.secondary">
-                                    Restaurant Fast-food - Burger
-                                </Typography>
-                                <Typography variant="body2" color="text.secondary">
-                                    {restaurant.address}
-                                </Typography>
-                            </CardContent>
-                        </CardActionArea>
-                    </Card>
-                </Grid>
+                    <Grid key={restaurant._id} item xs={3} sx={{ mx:6, my: 2 }}>
+                        <Card>
+                            <CardActionArea onClick={() => navigate('/Restaurant/' + restaurant._id)}>
+                                <CardMedia
+                                component="img"
+                                height="140"
+                                image={burgerImg}
+                                alt="Burger King"
+                                />
+                                <CardContent>
+                                    <Typography gutterBottom variant="h5" component="div">
+                                        {restaurant.name}
+                                    </Typography>
+                                    <Typography variant="body2" color="text.secondary">
+                                        Restaurant Fast-food - Burger
+                                    </Typography>
+                                    <Typography variant="body2" color="text.secondary">
+                                        {restaurant.address}
+                                    </Typography>
+                                </CardContent>
+                            </CardActionArea>
+                        </Card>
+                    </Grid>
                 ))}
             </Grid>
     )
--- Frontend/client/src/page/Restaurant.tsx
+++ Frontend/client/src/page/Restaurant.tsx
@@ -1,9 +1,48 @@
 import React from "react"
+import banniere from "../Assets/banniere-restaurant.jpg"
+import Navbar from "../components/nav/Navbar"
+import { Box, Grid, Typography } from "@mui/material"
+import Footer from "../components/nav/Footer";
+import Menus from "../components/Menus";
+import Articles from "../components/Articles";
+import axios from "axios";
+import { useParams } from "react-router-dom";
+import { useQuery } from "react-query";
 
 export default function Restaurant() {
+    const { id } = useParams();
+
+    const fetchRestaurant = async () => {
+        const {data} = await axios.get(`http://localhost/api/restaurants/${id}`);
+        return data;
+    }
+    const { data } = useQuery('restaurant', fetchRestaurant);
+    console.log(data);
+    
     return(
         <React.Fragment>
-            Test
+            <Navbar />
+            <Box sx={{ my:3, mx:2, justifyContent: 'center', alignItems: 'center', display: 'flex' }}>
+                <img src={banniere} alt="Bannière restaurant" width={1600} /> 
+            </Box>
+            <Grid container sx={{ ml: 'auto', mr: 'auto', mb:10, width: '90%', display: 'flex' }}>
+                <Grid item xs={12}>
+                    <Typography variant="h3">{data.name}</Typography>
+                </Grid>
+                <Grid item xs={12}>
+                    <Typography variant="body2" sx={{ mt:1 }}>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. </Typography>
+                    <Typography variant="body2" sx={{ mb: 3 }}>{data.address}</Typography>
+                </Grid>
+                <Grid item xs={12}>
+                    <Typography variant="h5" sx={{ my: 3 }}>Menu</Typography>
+                </Grid>
+                <Menus />
+                <Grid item xs={12}>
+                    <Typography variant="h5" sx={{ my: 3 }}>Articles</Typography>
+                </Grid>
+                <Articles />
+            </Grid>   
+            <Footer />    
         </React.Fragment>
     )
 }
